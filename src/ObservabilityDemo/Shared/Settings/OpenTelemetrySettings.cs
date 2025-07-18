e0429c8c0c03e3928fc05182fd62adee62c04999
Felix Notka <felix.notka@cyan-it.de>
2025-07-18 14:10:38 +0200

    Add OpenTelemetrySettings

--- src/ObservabilityDemo/Shared/Settings/OpenTelemetrySettings.cs
+++ src/ObservabilityDemo/Shared/Settings/OpenTelemetrySettings.cs
@@ -2,5 +2,11 @@
 
 public class OpenTelemetrySettings
 {
-
+    public static string Section = "OpenTelemetry";
+    public string Endpoint { get; set; } = "http://localhost:4318";
+    public string ServiceName { get; set; } = "ApplicationName";
+    public string ServiceVersion { get; set; } = "1.0.0";
+    public string ServiceEnvironment { get; set; } = "Development";
+    public string ServiceTenant { get; set; } = "Developers";
+    
 }
